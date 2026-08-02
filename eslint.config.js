e7b324df6834a11975659098fcb7684ddc3f021a
Andrei Provatorov <provatorovandrew@gmail.com>
2026-08-02 16:09:14 +0300

    chore: add lint tooling

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'no-empty': ['error', { allowEmptyCatch: true }],
+        },
+    },
+];
